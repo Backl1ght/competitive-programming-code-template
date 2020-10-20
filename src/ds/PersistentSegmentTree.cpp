228781f02eede255b4730a0cd907da0f1b452bb7
Backl1ght <1229309527@qq.com>
2020-10-21 00:44:04 +0800

    add segment tree

---
+++ src/ds/PersistentSegmentTree.cpp
@@ -0,0 +1,29 @@
+namespace Backlight {
+
+namespace PersistentSegmentTree {
+    using T = ll;
+
+    T s[N << 5];
+    int tot, L[N << 5], R[N << 5];
+
+    int update(int rt, int l, int r, int p) {
+        int nrt = ++tot;
+        L[nrt] = L[rt]; R[nrt] = R[rt]; s[nrt] = s[rt] + 1;
+        if (l != r) {
+            int mid = (l + r) >> 1;
+            if (p <= mid) L[nrt] = update(L[rt], l, mid, p);
+            else R[nrt] = update(R[rt], mid + 1, r, p);
+        }
+        return nrt;
+    }
+
+    int query(int u, int v, int l, int r, int k) {
+        if (l == r) return l;
+        int lsum = s[L[v]] - s[L[u]], mid = (l + r) >> 1;;
+        if (k <= lsum) return query(L[u], L[v], l, mid, k);
+        return query(R[u], R[v], mid + 1, r, k - lsum);
+    }
+} // namespace PersistentSegmentTree
+#define PST PersistentSegmentTree
+
+} // namespace Backlight
--- src/ds/segment_tree.cpp
+++ src/ds/segment_tree.cpp
@@ -1,13 +1,17 @@
+namespace Backlight {
+
 namespace SegmentTree {
-    #define ls (x<<1)
-    #define rs (x<<1|1)
-    #define mid ((l+r)>>1)
-    #define lson ls,l,mid
-    #define rson rs,mid+1,r
+    using T = long long;
+
+    #define ls (x << 1)
+    #define rs (x << 1 | 1)
+    #define mid ((l + r) >> 1)
+    #define lson ls, l, mid
+    #define rson rs, mid+1, r
 
     T v[N << 2];
 
-    void change(int x, int val) {
+    void change(int x, int l, int r, int val) {
 
     }
 
@@ -15,13 +19,13 @@ namespace SegmentTree {
 
     }
 
-    void push_down(int x) {
+    void push_down(int x, int l, int r) {
 
     }
 
-    void build(int x, int l, int r) {
+    void build(int x, int l, int r, T* a) {
         if (l == r) {
-            // do something here
+            v[x] = a[l];
             return;
         }
         build(lson); build(rson);
@@ -33,14 +37,14 @@ namespace SegmentTree {
             assert(l == p);
             return v[x];
         }
-        push_down(x);
+        push_down(x, l, r);
         if (p <= mid) return queryPoint(lson, p);
         return queryPoint(rson, p);
     }
 
     T querySegment(int x, int l, int r, int L, int R) {
         if (l == L && r == R) return v[x];
-        push_down(x);
+        push_down(x, l, r);
         if (R <= mid) return querySegment(lson, L, R);
         else if (L > mid) return querySegment(rson, L, R);
         return querySegment(lson, L, mid) + querySegment(rson, mid + 1, R);
@@ -49,10 +53,10 @@ namespace SegmentTree {
     void changePoint(int x, int l, int r, int p, int val) {
         if (l == r) {
             assert(l == p);
-            // make change hear
+            // make change here
             return;
         }
-        push_down(x);
+        push_down(x, l, r);
         if (p <= mid) changePoint(lson, p, val);
         else changePoint(rson, p, val);
         push_up(x);
@@ -60,10 +64,10 @@ namespace SegmentTree {
 
     void changeSegment(int x, int l, int r, int L, int R, int val) {
         if (l == L && r == R) {
-            // make change here
+            change(x, l, r, val);
             return;
         }
-        push_down(x);
+        push_down(x, l, r);
         if (R <= mid) changeSegment(lson, L, R, val);
         else if (L > mid) changeSegment(rson, L, R, val);
         else {
@@ -72,4 +76,6 @@ namespace SegmentTree {
         }
         push_up(x);
     }
-}
+} // namespace SegmentTree
+
+} // namespace Backlight
