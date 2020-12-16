ba5ea6d9a9567bd621dd3ef74ae56623b8d2e06b
Backl1ght <1229309527@qq.com>
2020-12-16 20:07:03 +0800

    update

--- src/graph/dsu-on-tree.cpp
+++ src/graph/dsu-on-tree.cpp
@@ -2,8 +2,8 @@
 // 对于每个节点，输出其子树中出现次数最多的颜色之和。
 vector<int> G[N];
 inline void addedge(int u, int v) {
-    G[u].PB(v);
-    G[v].PB(u);
+    G[u].push_back(v);
+    G[v].push_back(u);
 }
 
 int n, color[N];
@@ -47,4 +47,21 @@ void dfs2(int u, int fa, bool keep) {
         add(u, fa, -1, -1);
         ma = 0; cur = 0;
     }
+}
+
+void solve() {
+    read(n);
+    FOR(i, 1, n) read(color[i]);
+
+    int u, v;
+    FOR(i, 2, n) {
+        read(u, v);
+        addedge(u, v);
+    }
+
+    dfs1(1, 0);
+    dfs2(1, 0, 0);
+
+    FOR(i, 1, n - 1) printf("%lld ", ans[i]);
+    println(ans[n]);
 }
