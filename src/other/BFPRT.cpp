04a721a6e3296401ad109a6d5b16ede77fbb8fb2
backlight <1229309527@qq.com>
2021-04-19 16:42:01 +0800

    fix bug in BFPRT

--- src/other/BFPRT.cpp
+++ src/other/BFPRT.cpp
@@ -4,25 +4,31 @@
 **/
 namespace BFPRT {
     template<typename T, typename Cmp>
-    int medianOf3(T* a, int i, int j, int k, Cmp cmp) {
-        if (cmp(a[i], a[j])) {
-            if (cmp(a[j], a[k])) return j;
-            else if (cmp(a[i], a[k])) return k; 
-            else return i;
+    T kth_index(T* a, int l, int r, int k, Cmp cmp);
+
+    template<typename T, typename Cmp>
+    int insert_sort(T* a, int l, int r, Cmp cmp) {
+        for (int i = l + 1; i <= r; ++i) {
+            int tmp = a[i];
+            int j = i - 1;
+            while(j >= l && a[j] > tmp) {
+                a[j + 1] = a[j];
+                --j;
+            }
+            a[j + 1] = tmp;
         }
-        if (cmp(a[i], a[k])) return i;
-        else if (cmp(a[j], a[k])) return k;
-        else return j;
+        return l + (r - l) / 2;
     }
 
     template<typename T, typename Cmp>
     int pivot(T* a, int l, int r, Cmp cmp) {
+        if (r - l < 5) return insert_sort(a, l, r, cmp);
         int lst = l - 1;
-        for (int i = l; i + 2 <= r; i += 3) {
-            int p = medianOf3(a, i, i + 1, i + 2, cmp);
+        for (int i = l; i + 4 <= r; i += 5) {
+            int p = insert_sort(a, i, i + 4, cmp);
             swap(a[++lst], a[p]);
         }
-        return l + (lst - l) / 2;
+        return kth_index<T>(a, l, lst, (lst - l + 1) / 2 + 1, cmp);
     }
 
     template<typename T, typename Cmp>
@@ -31,23 +37,23 @@ namespace BFPRT {
         swap(a[p], a[r]);
         int lst = l - 1;
         for (int i = l; i < r; ++i) {
-            if (a[i] <= a[r]) swap(a[++lst], a[i]);
+            if (cmp(a[i], a[r])) swap(a[++lst], a[i]);
         }
         swap(a[++lst], a[r]);
         return lst;
     }
 
     template<typename T, typename Cmp>
-    T kth(T* a, int l, int r, int k, Cmp cmp) {
+    T kth_index(T* a, int l, int r, int k, Cmp cmp) {
         int p = partition(a, l, r, cmp);
         int d = p - l + 1;
-        if (d == k) return a[p];
-        else if (d < k) return kth(a, p + 1, r, k - d, cmp);
-        else return kth(a, l, p - 1, k, cmp);
+        if (d == k) return p;
+        else if (d < k) return kth_index(a, p + 1, r, k - d, cmp);
+        else return kth_index(a, l, p - 1, k, cmp);
     }
 
     template<typename T>
-    T kth(T* a, int l, int r, int k) {
-        return kth(a, l, r, k, less<T>());
+    T kth_index(T* a, int l, int r, int k) {
+        return kth_index(a, l, r, k, less<T>());
     }
 };
