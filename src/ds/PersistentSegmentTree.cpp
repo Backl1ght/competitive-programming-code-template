b36001babe7a8f050d1eafe6cb3847e49a39d922
backlight <1229309527@qq.com>
2021-04-30 22:03:14 +0800

    update

--- src/ds/PersistentSegmentTree.cpp
+++ src/ds/PersistentSegmentTree.cpp
@@ -1,25 +1,42 @@
-namespace PersistentSegmentTree {
-    using T = ll;
+struct PersistentSegmentTree
+{
+// $SIZE = N \log N$
+#define SIZE 200005 * 20
 
-    T s[N << 5];
-    int tot, L[N << 5], R[N << 5];
+    int tot;
+    int c[SIZE];
+    int L[SIZE], R[SIZE];
 
-    int update(int rt, int l, int r, int p) {
+    PersistentSegmentTree() { tot = 0; }
+
+    int update(int rt, int l, int r, int p, int d)
+    {
         int nrt = ++tot;
-        L[nrt] = L[rt]; R[nrt] = R[rt]; s[nrt] = s[rt] + 1;
-        if (l != r) {
+        L[nrt] = L[rt];
+        R[nrt] = R[rt];
+        c[nrt] = c[rt] + d;
+
+        if (l != r)
+        {
             int mid = (l + r) >> 1;
-            if (p <= mid) L[nrt] = update(L[rt], l, mid, p);
-            else R[nrt] = update(R[rt], mid + 1, r, p);
+            if (p <= mid)
+                L[nrt] = update(L[rt], l, mid, p, d);
+            else
+                R[nrt] = update(R[rt], mid + 1, r, p, d);
         }
+
         return nrt;
     }
 
     // 区间第k小
-    int query(int u, int v, int l, int r, int k) {
-        if (l == r) return l;
-        int lsum = s[L[v]] - s[L[u]], mid = (l + r) >> 1;;
-        if (k <= lsum) return query(L[u], L[v], l, mid, k);
-        return query(R[u], R[v], mid + 1, r, k - lsum);
+    int query(int u, int v, int l, int r, int k)
+    {
+        if (l == r)
+            return l;
+        int left_size = c[L[v]] - c[L[u]];
+        int mid = (l + r) >> 1;
+        if (k <= left_size)
+            return query(L[u], L[v], l, mid, k);
+        return query(R[u], R[v], mid + 1, r, k - left_size);
     }
-}
+};
