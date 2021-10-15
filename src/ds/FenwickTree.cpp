26b1606f6d3a1b18971478254f4a38402e983e3e
backlight <1229309527@qq.com>
2021-10-15 23:20:02 +0800

    fix(ds/FenwickTree): fix up bound and replace log2 with __lg

--- src/ds/FenwickTree.cpp
+++ src/ds/FenwickTree.cpp
@@ -4,12 +4,12 @@ struct FenwickTree {
     vector<T> c;
     FenwickTree(int _n) : n(_n), c(n + 1) {}
     inline int lb(int x) { return x & -x; }
-    void add(int x, T d) { for (; x < n; x += lb(x)) c[x] += d; }
+    void add(int x, T d) { for (; x <= n; x += lb(x)) c[x] += d; }
     T getsum(int x) { T r = 0; for (; x; x -= lb(x)) r += c[x]; return r; }
     T getsum(int l, int r) { return getsum(r) - getsum(l - 1); }
     T kth(int k) {
         T ans = 0, cnt = 0;
-        for (int i = log2(n) + 1; i >= 0; --i) {
+        for (int i = __lg(n) + 1; i >= 0; --i) {
             ans += (1LL << i);
             if (ans >= n || cnt + c[ans] >= k) ans -= (1LL << i);
             else cnt += c[ans];
