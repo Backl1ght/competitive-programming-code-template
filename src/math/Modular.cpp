df6d09f06bcd8cbca1543d3155f0657ab956ebe8
Backl1ght <1229309527@qq.com>
2022-03-19 18:27:56 +0800

    fix(math/Modular.cpp): add quick power

--- src/math/Modular.cpp
+++ src/math/Modular.cpp
@@ -8,6 +8,16 @@ int mul(int x, int y) {
 int sub(int x, int y) {
   return x - y < 0 ? x - y + MOD : x - y;
 }
+int qp(int x, int y) {
+  int r = 1;
+  while (y) {
+    if (y & 1)
+      r = mul(r, x);
+    x = mul(x, x);
+    y >>= 1;
+  }
+  return r;
+}
 int dvd(int x, int y) {
   return 1ll * x * qp(y, MOD - 2) % MOD;
 }
