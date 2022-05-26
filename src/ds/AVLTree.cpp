7a91aefb42c669b6fce56439c3d4acd71e7d7e23
Backl1ght <1229309527@qq.com>
2022-05-26 09:34:45 +0800

    refactor(ds/avl): refine impl

--- src/ds/AVLTree.cpp
+++ src/ds/AVLTree.cpp
@@ -1,282 +1,316 @@
+// Problem: P6136 【模板】普通平衡树（数据加强版）
+// Contest: Luogu
+// URL: https://www.luogu.com.cn/problem/P6136
+// Memory Limit: 88 MB
+// Time Limit: 3000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+
 #include <bits/stdc++.h>
-using namespace std;
 
-#define FOR(i, l, r) for (int i = l; i <= r; ++i)
-#define ROF(i, r, l) for (int i = r; i >= l; --i)
-#define REP(i, l, r) for (int i = l; i < r; ++i)
-#define PER(i, r, l) for (int i = r - 1; i >= l; --i)
+#define CPPIO std::ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
+#define freep(p) p ? delete p, p = nullptr, void(1) : void(0)
 
-const int N = 1e5 + 5;
-using ll = long long;
+#ifdef BACKLIGHT
+#include "debug.h"
+#else
+#define logd(...) ;
+#endif
 
-template <typename T>
-struct AVLTree {
-  struct node {
-    T v;
-    int sz, h, cnt;
-    node *l, *r;
-    explicit node(T _v)
-        : v(_v) {
-      sz = h = cnt = 1;
-      l = r = nullptr;
+using i64 = int64_t;
+using u64 = uint64_t;
+
+void solve_case(int Case);
+
+int main() {
+  CPPIO;
+  int T = 1;
+  // std::cin >> T;
+  for (int t = 1; t <= T; ++t) {
+    solve_case(t);
+  }
+  return 0;
+}
+
+template <typename ValueType>
+class AVLTree {
+ private:
+  struct Node {
+    ValueType value;
+    size_t count;
+    size_t size;
+    size_t height;
+    Node* left;
+    Node* right;
+    Node() {}
+    Node(ValueType value) {
+      this->value = value;
+      this->count = this->size = 1;
+      this->height = 1;
+      this->left = this->right = nullptr;
     }
   };
-  node* root = nullptr;
-  int get_size(node* p) {
-    return p ? p->sz : 0;
-  }
 
-  int get_height(node* p) {
-    return p ? p->h : 0;
-  }
+  size_t GetSize(Node* p) { return p ? p->size : 0; }
 
-  void push_up(node* p) {
+  size_t GetHeight(Node* p) { return p ? p->height : 0; }
+
+  void Maintain(Node* p) {
     if (!p)
       return;
-    p->sz = get_size(p->l) + p->cnt + get_size(p->r);
-    p->h = 1 + max(get_height(p->l), get_height(p->r));
+    p->size = GetSize(p->left) + p->count + GetSize(p->right);
+    p->height = std::max(GetHeight(p->left), GetHeight(p->right)) + 1;
   }
 
-  void zig(node*& p) {
-    node* q = p;
-    q = p->r;
-    p->r = q->l;
-    q->l = p;
-    push_up(p);
-    push_up(q);
-    p = q;
+  void Zig(Node*& p) {
+    Node* r = p->right;
+    p->right = r->left;
+    r->left = p;
+    Maintain(p);
+    Maintain(r);
+    p = r;
   }
 
-  void zag(node*& p) {
-    node* q = p->l;
-    p->l = q->r;
-    q->r = p;
-    push_up(p);
-    push_up(q);
-    p = q;
+  void Zag(Node*& p) {
+    Node* l = p->left;
+    p->left = l->right;
+    l->right = p;
+    Maintain(p);
+    Maintain(l);
+    p = l;
   }
 
-  void maintain(node*& p) {
+  void KeepBalance(Node*& p) {
     if (!p)
       return;
-    if (get_height(p->l) - get_height(p->r) == -2) {
-      if (p->r && get_height(p->r->l) > get_height(p->r->r)) {
-        zag(p->r);
+    if (GetHeight(p->left) - GetHeight(p->right) == -2) {
+      if (p->right && GetSize(p->right->left) > GetSize(p->right->right)) {
+        Zag(p->right);
       }
-      zig(p);
-      return;
-    }
-    if (get_height(p->l) - get_height(p->r) == 2) {
-      if (p->l && get_height(p->l->l) < get_height(p->l->r)) {
-        zig(p->l);
+      Zig(p);
+    } else if (GetHeight(p->left) - GetHeight(p->right) == 2) {
+      if (p->left && GetSize(p->left->left) < GetSize(p->left->right)) {
+        Zig(p->left);
       }
-      zag(p);
-      return;
+      Zag(p);
     }
+    Maintain(p);
   }
 
-  void ins(node*& p, T v) {
+  void InsertInternal(Node*& p, ValueType value) {
     if (!p) {
-      p = new node(v);
+      p = new Node(value);
       return;
     }
-    if (p->v == v) {
-      ++(p->cnt);
+    if (value < p->value) {
+      InsertInternal(p->left, value);
+    } else if (value == p->value) {
+      p->count++;
     } else {
-      if (v < p->v) {
-        ins(p->l, v);
-      } else {
-        ins(p->r, v);
-      }
+      InsertInternal(p->right, value);
     }
-    push_up(p);
-    maintain(p);
-    push_up(p);
+    Maintain(p);
+    KeepBalance(p);
   }
 
-  void del(node*& p, T v) {
+  void DeleteInternal(Node*& p, ValueType value) {
     if (!p)
       return;
-    if (p->v == v) {
-      --p->cnt;
-      if (p->cnt == 0) {
-        if (p->l && p->r) {
-          node* q = p->r;
-          while (q->l)
-            q = q->l;
-          p->cnt = q->cnt, p->v = q->v;
-          q->cnt = 1;
-          del(p->r, q->v);
+    if (value < p->value) {
+      DeleteInternal(p->left, value);
+    } else if (value == p->value) {
+      if (p->left && p->right) {
+        Node* replace = p->right;
+        while (replace->left)
+          replace = replace->left;
+        p->value = replace->value;
+        p->count = replace->count;
+        replace->count = 1;
+        DeleteInternal(p->right, replace->value);
+      } else {
+        Node* temp = p;
+        if (p->left)
+          p = p->left;
+        else if (p->right)
+          p = p->right;
+        else
+          p = nullptr;
+        freep(temp);
+      }
+    } else {
+      DeleteInternal(p->right, value);
+    }
+    Maintain(p);
+    KeepBalance(p);
+  }
+
+  void DeleteOneInternal(Node*& p, ValueType value) {
+    if (!p)
+      return;
+    if (value < p->value) {
+      DeleteOneInternal(p->left, value);
+    } else if (value == p->value) {
+      --p->count;
+      if (p->count == 0) {
+        if (p->left && p->right) {
+          Node* replace = p->right;
+          while (replace->left)
+            replace = replace->left;
+          p->value = replace->value;
+          p->count = replace->count;
+          replace->count = 1;
+          DeleteInternal(p->right, replace->value);
         } else {
-          node* q = p;
-          if (p->l)
-            p = p->l;
-          else if (p->r)
-            p = p->r;
+          Node* temp = p;
+          if (p->left)
+            p = p->left;
+          else if (p->right)
+            p = p->right;
           else
             p = nullptr;
-          delete q;
-          q = nullptr;
+          freep(temp);
         }
       }
     } else {
-      if (v < p->v)
-        del(p->l, v);
-      else
-        del(p->r, v);
+      DeleteOneInternal(p->right, value);
     }
-    push_up(p);
-    maintain(p);
-    push_up(p);
+    Maintain(p);
+    KeepBalance(p);
+    Maintain(p);
   }
 
-  void ins(T v) {
-    ins(root, v);
+  ValueType GetKthInternal(Node* p, size_t k) {
+    if (k <= 0)
+      assert(false);
+    if (k <= GetSize(p->left))
+      return GetKthInternal(p->left, k);
+    else if (k <= GetSize(p->left) + p->count)
+      return p->value;
+    else
+      return GetKthInternal(p->right, k - GetSize(p->left) - p->count);
   }
 
-  void del(T v) {
-    del(root, v);
-  }
-
-  int getRank(T v) {
-    node* p = root;
-    int res = 0;
-    while (p) {
-      if (v == p->v) {
-        res += get_size(p->l);
-        break;
-      }
-      if (v < p->v)
-        p = p->l;
-      else {
-        res += get_size(p->l) + p->cnt;
-        p = p->r;
-      }
-    }
-    return res + 1;
-  }
-
-  T getKth(int k) {
-    node* p = root;
-    T res = -1;
-    while (p) {
-      if (k <= get_size(p->l))
-        p = p->l;
-      else if (k - get_size(p->l) <= p->cnt) {
-        res = p->v;
-        break;
-      } else {
-        k -= get_size(p->l) + p->cnt;
-        p = p->r;
-      }
-    }
-    return res;
-  }
-
-  T getPrev(T v) {
-    T res = numeric_limits<T>::min();
-    node* p = root;
-    while (p) {
-      if (v == p->v) {
-        node* q = p->l;
-        if (q) {
-          while (q->r)
-            q = q->r;
-          res = q->v;
-        }
-        break;
-      }
-
-      if (v < p->v) {
-        p = p->l;
-      } else {
-        if (p->v > res)
-          res = p->v;
-        p = p->r;
-      }
-    }
-    return res;
-  }
-
-  T getSucc(T v) {
-    T res = numeric_limits<T>::max();
-    node* p = root;
-    while (p) {
-      if (v == p->v) {
-        node* q = p->r;
-        if (q) {
-          while (q->l)
-            q = q->l;
-          res = q->v;
-        }
-        break;
-      }
-
-      if (v < p->v) {
-        if (p->v < res)
-          res = p->v;
-        p = p->l;
-      } else {
-        p = p->r;
-      }
-    }
-    return res;
-  }
-
-  void debug(node* p) {
+  size_t GetPrevRank(Node* p, ValueType value) {
     if (!p)
-      return;
-    debug(p->l);
-    cerr << p->v << " ";
-    debug(p->r);
+      return 0;
+    if (value < p->value) {
+      return GetPrevRank(p->left, value);
+    } else if (value == p->value) {
+      return GetSize(p->left);
+    } else {
+      return GetSize(p->left) + p->count + GetPrevRank(p->right, value);
+    }
   }
 
-  void debug() {
-    cerr << "INORDER: " << endl;
-    debug(root);
-    cerr << endl;
+  size_t GetRankInternal(Node* p, ValueType value) { return GetPrevRank(p, value) + 1; }
+
+  size_t GetNextRank(Node* p, ValueType value) {
+    if (!p)
+      return 1;
+    if (value < p->value) {
+      return GetNextRank(p->left, value);
+    } else if (value == p->value) {
+      return GetSize(p->left) + p->count + 1;
+    } else {
+      return GetSize(p->left) + p->count + GetNextRank(p->right, value);
+    }
+  }
+
+ private:
+  Node* root_;
+
+ public:
+  AVLTree() : root_(nullptr) {}
+
+  ~AVLTree() {
+    std::function<void(Node*)> dfs = [&](Node* p) -> void {
+      if (!p)
+        return;
+      dfs(p->left);
+      dfs(p->right);
+      freep(p);
+    };
+    dfs(root_);
+  }
+
+  void Insert(ValueType value) { InsertInternal(root_, value); }
+
+  void Delete(ValueType value) { DeleteInternal(root_, value); }
+
+  void DeleteOne(ValueType value) { DeleteOneInternal(root_, value); }
+
+  size_t GetRank(ValueType value) { return GetRankInternal(root_, value); }
+
+  ValueType GetKth(size_t k) { return GetKthInternal(root_, k); }
+
+  ValueType GetPrev(ValueType value) {
+    size_t prev_rank = GetPrevRank(root_, value);
+    return GetKthInternal(root_, prev_rank);
+  }
+
+  ValueType GetNext(ValueType value) {
+    size_t next_rank = GetNextRank(root_, value);
+    return GetKthInternal(root_, next_rank);
+  }
+
+  std::string ToString() {
+    std::string result;
+    std::function<void(Node*)> dfs = [&](Node* p) {
+      if (!p)
+        return;
+      dfs(p->left);
+      result += "(" + std::to_string(p->value) + "," + std::to_string(p->count) + "), ";
+      dfs(p->right);
+    };
+    dfs(root_);
+    return result;
   }
 };
 
-void solve(int Case) {
-  int n;
-  scanf("%d", &n);
-  int op, x;
+void solve_case(int Case) {
+  int n, q;
+  std::cin >> n >> q;
+
   AVLTree<int> t;
-  FOR(i, 1, n) {
-    scanf("%d %d", &op, &x);
-    //        cerr << op << " " << x << endl;
+  for (int i = 1, x; i <= n; ++i) {
+    std::cin >> x;
+    t.Insert(x);
+  }
+
+  int ans = 0;
+  for (int i = 1, op, x, last = 0; i <= q; ++i) {
+    // std::cerr << t.ToString() << std::endl;
+    std::cin >> op >> x;
+    x ^= last;
     switch (op) {
       case 1:
-        t.ins(x);
+        t.Insert(x);
         break;
       case 2:
-        t.del(x);
+        t.DeleteOne(x);
         break;
       case 3:
-        printf("%d\n", t.getRank(x));
+        last = t.GetRank(x);
+        ans ^= last;
+        logd(last);
         break;
       case 4:
-        printf("%d\n", t.getKth(x));
+        last = t.GetKth(x);
+        ans ^= last;
+        logd(last);
         break;
       case 5:
-        printf("%d\n", t.getPrev(x));
+        last = t.GetPrev(x);
+        ans ^= last;
+        logd(last);
         break;
       case 6:
-        printf("%d\n", t.getSucc(x));
+        last = t.GetNext(x);
+        ans ^= last;
+        logd(last);
         break;
     }
-    //        t.debug();
+    // std::cerr << t.ToString() << std::endl;
   }
-}
-
-int main() {
-#ifdef BACKLIGHT
-  freopen("a.in", "r", stdin);
-#endif
-  int T = 1;
-  //    scanf("%d", &T);
-  for (int _ = 1; _ <= T; ++_)
-    solve(_);
-  return 0;
+  std::cout << ans << "\n";
 }
