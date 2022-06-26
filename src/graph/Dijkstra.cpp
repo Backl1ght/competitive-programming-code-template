bc8d5af417f04f0395f8fe0ee7eb88fa53dbde71
Backl1ght <1229309527@qq.com>
2022-06-26 10:16:02 +0800

    refactor(graph/dijkstra): refactor dijkstra

--- src/graph/Dijkstra.cpp
+++ src/graph/Dijkstra.cpp
@@ -1,62 +1,37 @@
-namespace Backlight {
-
-template <typename T>
-struct Wraph {
-  struct Edge {
-    int u, v;
-    T w;
-    Edge() {}
-    Edge(int _u, int _v, T _w)
-        : u(_u), v(_v), w(_w) {}
-  };
-
-  int V;
-  vector<vector<Edge>> G;
-
-  Wraph()
-      : V(0) {}
-  Wraph(int _V)
-      : V(_V), G(_V + 1) {}
-
-  inline void addarc(int u, int v, T w) {
-    assert(1 <= u && u <= V);
-    assert(1 <= v && v <= V);
-    G[u].push_back(Edge(u, v, w));
-  }
-
-  inline void addedge(int u, int v, T w) {
-    addarc(u, v, w);
-    addarc(v, u, w);
-  }
-
-  /**************************************************/
-  vector<T> dijkstra(int S, T T_MAX) {
-    typedef pair<T, int> Node;
-    priority_queue<Node, vector<Node>, greater<Node>> q;
-    vector<T> dis(V + 1);
-    for (int i = 1; i <= V; i++)
-      dis[i] = T_MAX;
-    dis[S] = 0;
-    q.push(Node(0, S));
-    while (!q.empty()) {
-      Node p = q.top();
-      q.pop();
-      T cost = p.first;
-      int u = p.second;
-      if (dis[u] != cost)
-        continue;
-
-      for (Edge e : G[u]) {
-        int v = e.v;
-        T w = e.w;
-        if (dis[v] > dis[u] + w) {
-          dis[v] = dis[u] + w;
-          q.push(Node(dis[v], v));
-        }
-      }
-    }
-    return dis;
-  }
+template <typename DistanceType>
+struct Edge {
+  int v_;
+  DistanceType w_;
+  Edge() {}
+  Edge(int v, DistanceType w) : v_(v), w_(w) {}
 };
 
-}  // namespace Backlight
+template <typename DistanceType>
+std::vector<DistanceType> Dijkstra(const std::vector<std::vector<Edge<DistanceType>>>& g, int s) {
+  using Node = std::pair<DistanceType, int>;
+  const DistanceType INF = std::numeric_limits<DistanceType>::max();
+  const int n = g.size();
+
+  std::vector<DistanceType> dis(n, INF);
+  std::vector<bool> vis(n, false);
+
+  std::priority_queue<Node, std::vector<Node>, std::greater<Node>> q;
+  dis[s] = 0;
+  q.push(Node(dis[s], s));
+  while (!q.empty()) {
+    auto [c, u] = q.top();
+    q.pop();
+
+    if (vis[u])
+      continue;
+    vis[u] = true;
+
+    for (auto [v, w] : g[u]) {
+      if (dis[v] > c + w) {
+        dis[v] = c + w;
+        q.push(Node(dis[v], v));
+      }
+    }
+  }
+  return dis;
+}
