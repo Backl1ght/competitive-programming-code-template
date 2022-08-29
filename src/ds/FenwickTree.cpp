12d4790f6ff515d7f9005fa194964f9470b227a6
Backl1ght <1229309527@qq.com>
2022-08-30 02:19:36 +0800

    feat(ds/fenwick): add timestamp and reset

--- src/ds/FenwickTree.cpp
+++ src/ds/FenwickTree.cpp
@@ -1,30 +1,58 @@
 template <typename T>
-struct FenwickTree {
-  int n;
-  vector<T> c;
-  FenwickTree(int _n)
-      : n(_n), c(n + 1) {}
+class FenwickTree {
+ public:
+  using Operator = std::function<T(const T&, const T&)>;
+
+ private:
   inline int lb(int x) { return x & -x; }
-  void add(int x, T d) {
-    for (; x <= n; x += lb(x))
-      c[x] += d;
+
+ public:
+  FenwickTree(int n, const T& init, const Operator& op = std::plus<T>())
+      : n_(n), init_(init), c_(n_ + 1, init_), op_(op), t_(n_ + 1, -1), tag_(0) {}
+
+  void Update(int x, T d) {
+    for (; x <= n_; x += lb(x)) {
+      if (t_[x] != tag_)
+        c_[x] = init_;
+      c_[x] = op_(c_[x], d);
+      t_[x] = tag_;
+    }
   }
-  T getsum(int x) {
-    T r = 0;
-    for (; x; x -= lb(x))
-      r += c[x];
+
+  const T Query(int x) {
+    T r = init_;
+    for (; x; x -= lb(x)) {
+      if (t_[x] == tag_)
+        r = op_(r, c_[x]);
+    }
     return r;
   }
-  T getsum(int l, int r) { return getsum(r) - getsum(l - 1); }
-  T kth(int k) {
+
+  // TODO(backlight): add something to ensure that this can only be called when op_ is inversable.
+  const T Query(int l, int r) { return Query(r) - Query(l - 1); }
+
+  // TODO(backlight): similar to range query.
+  const T Kth(int k) {
     T ans = 0, cnt = 0;
-    for (int i = __lg(n) + 1; i >= 0; --i) {
+    for (int i = std::__lg(n_) + 1; i >= 0; --i) {
       ans += (1LL << i);
-      if (ans >= n || cnt + c[ans] >= k)
+      if (ans >= n_ || cnt + c_[ans] >= k)
         ans -= (1LL << i);
       else
-        cnt += c[ans];
+        cnt += c_[ans];
     }
     return ans + 1;
   }
+
+  void Reset() { ++tag_; }
+
+ private:
+  int n_;
+
+  T init_;
+  std::vector<T> c_;
+  const Operator op_;
+
+  std::vector<int> t_;
+  int tag_;
 };
